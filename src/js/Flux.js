196e9128abb1893c30312b08ef183b7714180b81
Alexandre Dufour <adufour@insa-lyon.fr>
2020-03-22 12:18:27 +0100

    Classe Flux, échanges TCP, stockage des données

--- src/js/Flux.js
+++ src/js/Flux.js
@@ -1,43 +1,134 @@
+// Require des modules node
+var net = require('net');
+
 //classe qui permet de gérer les données liées à une mesure et une cible
 class Flux {
 
     //attributs;
-    ID;             //id unique du flux
-    groupe;         //nom du barrage et groupe de turbine du flux
-    attribute;      //nom de la mesure qui concerne ce flux
-    address;        //adresse sur le serveur ou trouver les données liées a la mesure
-    port;           //port du serveur ou trouver les données liées a la mesure
-    type;           //temps réel (RT) ou historique (H)
-    protocol;       //nom du protocole pour obtenir les données
-    frequency;      //fréquence de rafraichissement des données
+    #ID;             //id unique du flux
+    #groupe;         //nom du barrage et groupe de turbine du flux
+    #attribute;      //nom de la mesure qui concerne ce flux
+    #address;        //adresse sur le serveur ou trouver les données liées a la mesure
+    #port;           //port du serveur ou trouver les données liées a la mesure
+    #type;           //temps réel (RT) ou historique (H)
+    #protocol;       //nom du protocole pour obtenir les données
+    #frequency;      //fréquence de rafraichissement des données
+    #donnees;        // Données récupérees sur ce flux, paires <timestamp, valeur>
+    #client;         // Socket TCP qui va faire les requêtes pour récupérer les données
+    #scheduler;      // ID du scheduler qui envoie des requêtes à intervales réguliers
 
     //constructeur
-    // id => id unique caractérisant le flux
-    // mesure => nom de la mesure
-    // adresse => adresse ou trouver les données sur le serveur
     constructor(ID, groupe, attribute, address, port, type, protocol, frequency) {
-        this.ID = ID;
-        this.groupe = groupe;
-        this.attribute = attribute;
-        this.address = address;
-        this.port = port;
-        this.type = type;
-        this.protocol = protocol;
-        this.frequency = frequency;
+        this.ID = ID;                   
+        this.groupe = groupe;          
+        this.attribute = attribute;     
+        this.address = address;         
+        this.port = port;               
+        this.type = type;               
+        this.protocol = protocol;       
+        this.frequency = frequency;     
+        this.donnees = new Map();  
+        this.scheduler = -1;     
     }
 
-    // Récupere sur le serveur les données liées au flux entre les deux dates indiquée entre parametre
-    // dateDebut => date de debut de la recuperation des données
-    // dateFin => date de fin de la recuperation des données. Si non indiqué, la date de fin est la date actuelle
-    RecupererDonnees(dateDebut, dateFin) {
-        
-        var res;
+    // Getters
+    GetID() {return (this.ID);}
+    GetGroupe() {return (this.groupe);}
+    GetAttribute() {return (this.attribute);}
+    GetAddress() {return (this.address);}
+    GetPort() {return (this.port);}
+    GetType() {return (this.type);}
+    GetProtocol() {return (this.protocol);}
+    GetFrequency() {return (this.frequency);}
 
-        //TODO : traitement permettant de récupérer les données associées a l'adresse du flux
-
-        this.donnees = res;
+    GetDonnees(debut, fin) {
+        var dateDebut;
+        var dateFin;
+        // Check debut
+        if (typeof debut == "string")
+            dateDebut = ParseDate(debut);
+        else if (debut instanceof Date)
+            dateDebut = debut;
+        else if (typeof debut == "number")
+            dateDebut = new Date(debut);
+        else
+            dateDebut = null;
+        // Check fin
+        if (typeof fin == "string")
+            dateFin = ParseDate(fin);
+        else if (fin instanceof Date)
+            dateFin = fin;
+        else if (typeof fin == "number")
+            dateFin = new Date(fin);
+        else
+            dateFin = null;
+        // Check des deux
+        if (dateFin == null || dateDebut == null)
+            return null;
+        // Check ok
+        var retour = new Map();
+        for (const [date, valeur] of this.donnees) {
+            if (date >= dateDebut && date <= dateFin) {
+                retour.set(date, valeur);
+            }
+        }
+        return (retour);
     }
 
+    // Autres méthodes
+    Connexion() {
+        const flux = this;
+        this.client = net.createConnection({allowHalfOpen: true, host: this.address, port: this.port}, () => {
+            console.log('connected to port ' + this.port);
+        });
+        this.client.on('data', (data) => {
+            flux.AddDonnee("" + data);
+        });
+    }
+
+    Deconnexion() {
+        if (this.scheduler != -1)
+            this.StopScheduler();
+        this.client.write('END\r\n\r\n');
+    }
+
+    StartScheduler() {
+        var periode = parseFloat(this.frequency) * 1000;
+        const flux = this;
+        this.scheduler = setInterval(()=> {
+            flux.client.write('GET\r\n\r\n');
+        } , periode);
+    }
+
+    StopScheduler() {
+        if (this.scheduler != -1) {
+            clearInterval(this.scheduler);
+            this.scheduler = -1;
+        }
+    }
+
+    AddDonnee(donnee) {
+        var splitted = donnee.split(" ");
+        var date  = ParseDate(splitted[0] + ' ' + splitted[1]);
+        if (!this.donnees.has(date)) {
+            var valeur = parseFloat(splitted[2]);
+            this.donnees.set(date, valeur);
+        }
+    }
 }
 
+// Fonction qui parse la date au format du serveur en js Date
+function ParseDate(chaineDate) {
+    regexDate = '^[0-9][0-9]/[0-9][0-9]/[0-9][0-9][0-9][0-9] [0-9][0-9]:[0-9][0-9]:[0-9][0-9]';
+    if (!chaineDate.match(regexDate))
+        return null;
+    jour = chaineDate.slice(0, 2);
+    mois = chaineDate.slice(3, 5);
+    annee = chaineDate.slice(6, 10);
+    heure = chaineDate.slice(-8);
+    nouvelleChaine = annee + '-' + mois + '-' + jour + 'T' + heure;
+    return (new Date(nouvelleChaine));
+}   
+
 module.exports.Flux = Flux;
+module.exports.ParseDate = ParseDate;
--- src/js/test_connexion_serveur/app.js
+++ src/js/test_connexion_serveur/app.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var com = require('../Catalogue.js')
+var flux = require('../Flux.js')
 
 
 
@@ -24,9 +25,23 @@ app.CatalogueFlux = [];
 module.exports.app = app;
 module.exports.getApp = getApp;
 module.exports.text = "coucou";
+/*
+debut = '22/03/2020 10:17:50';
+fin = '22/03/2020 10:19:50'
+dateDebut = flux.ParseDate(debut);
+dateFin = flux.ParseDate(fin);
+*/
 
 app.ComServeur.recupererFichierCatalogue();
 setTimeout(function() {
-	console.log(app.ComServeur);
-	while (1);
-} , 3000);
+	var fluxxx = app.ComServeur.GetFlux()[0];
+	fluxxx.Connexion();
+	fluxxx.StartScheduler();
+	setTimeout(function() {
+		fluxxx.StopScheduler();
+		console.log(fluxxx.GetDonnees('22/03/2020 00:00:00', Date.now()));
+		fluxxx.Deconnexion();
+		while(1);
+	}, 1500)
+} , 1000);
+
